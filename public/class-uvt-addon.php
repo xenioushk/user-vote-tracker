1231f74dfb2edc0bcc8c6ac74203d6a01a0cedc5
Md Mahbub Alam Khan <hkhan.cse@gmail.com>
2023-07-16 15:47:58 +0300

    removed doc

--- public/class-uvt-addon.php
+++ public/class-uvt-addon.php
@@ -1,13 +1,15 @@
 <?php
 
-class BPVM_UVT {
+class BPVM_UVT
+{
 
     const VERSION = '1.0.0';
 
     protected $plugin_slug = 'bpvm-uvt';
     protected static $instance = null;
 
-    private function __construct() {
+    private function __construct()
+    {
 
         if (class_exists('BWL_Pro_Voting_Manager') && BPVM_UVT_PARENT_PLUGIN_INSTALLED_VERSION > '1.1.3') {
 
@@ -28,18 +30,21 @@ class BPVM_UVT {
         }
     }
 
-    function get_user_vote_data() {
+    function get_user_vote_data()
+    {
 
         echo $uvt_vote_data = $this->uvt_get_data($_POST['start_id'], $_POST['filter'], $_POST['limit'], $_POST['pagination'], $_POST['meta'], $_POST['global_mode']);
 
         die();
     }
 
-    public function include_files() {
-        require_once( BPVM_UVT_DIR . 'widgets/user-vote-tracker-widget.php' );
+    public function include_files()
+    {
+        require_once(BPVM_UVT_DIR . 'widgets/user-vote-tracker-widget.php');
     }
 
-    public function uvt_create_custom_column() {
+    public function uvt_create_custom_column()
+    {
 
 
         // First we need to check if the patch already applied or not.
@@ -70,7 +75,8 @@ class BPVM_UVT {
      *
      * @return    Plugin slug variable.
      */
-    public function get_plugin_slug() {
+    public function get_plugin_slug()
+    {
         return $this->plugin_slug;
     }
 
@@ -81,7 +87,8 @@ class BPVM_UVT {
      *
      * @return    object    A single instance of this class.
      */
-    public static function get_instance() {
+    public static function get_instance()
+    {
 
         // If the single instance hasn't been set, set it now.
         if (null == self::$instance) {
@@ -101,7 +108,8 @@ class BPVM_UVT {
      *                                       WPMU is disabled or plugin is
      *                                       activated on an individual blog.
      */
-    public static function activate($network_wide) {
+    public static function activate($network_wide)
+    {
 
         if (function_exists('is_multisite') && is_multisite()) {
 
@@ -135,7 +143,8 @@ class BPVM_UVT {
      *                                       WPMU is disabled or plugin is
      *                                       deactivated on an individual blog.
      */
-    public static function deactivate($network_wide) {
+    public static function deactivate($network_wide)
+    {
 
         if (function_exists('is_multisite') && is_multisite()) {
 
@@ -158,8 +167,9 @@ class BPVM_UVT {
             self::single_deactivate();
         }
     }
-    
-    public function activate_new_site($blog_id) {
+
+    public function activate_new_site($blog_id)
+    {
 
         if (1 !== did_action('wpmu_new_blog')) {
             return;
@@ -169,8 +179,9 @@ class BPVM_UVT {
         self::single_activate();
         restore_current_blog();
     }
-    
-    private static function get_blog_ids() {
+
+    private static function get_blog_ids()
+    {
 
         global $wpdb;
 
@@ -187,7 +198,8 @@ class BPVM_UVT {
      *
      * @since    1.0.0
      */
-    private static function single_activate() {
+    private static function single_activate()
+    {
         // @TODO: Define activation functionality here
     }
 
@@ -196,7 +208,8 @@ class BPVM_UVT {
      *
      * @since    1.0.0
      */
-    private static function single_deactivate() {
+    private static function single_deactivate()
+    {
         // @TODO: Define deactivation functionality here
     }
 
@@ -205,7 +218,8 @@ class BPVM_UVT {
      *
      * @since    1.0.0
      */
-    public function load_plugin_textdomain() {
+    public function load_plugin_textdomain()
+    {
 
         $domain = $this->plugin_slug;
         $locale = apply_filters('plugin_locale', get_locale(), $domain);
@@ -218,7 +232,8 @@ class BPVM_UVT {
      *
      * @since    1.0.0
      */
-    public function enqueue_styles() {
+    public function enqueue_styles()
+    {
         wp_enqueue_style($this->plugin_slug . '-plugin-styles', plugins_url('assets/css/public.css', __FILE__), array(), self::VERSION);
     }
 
@@ -227,11 +242,13 @@ class BPVM_UVT {
      *
      * @since    1.0.0
      */
-    public function enqueue_scripts() {
+    public function enqueue_scripts()
+    {
         wp_enqueue_script($this->plugin_slug . '-plugin-script', plugins_url('assets/js/public.js', __FILE__), array('jquery'), self::VERSION);
     }
 
-    public function uvt_get_data($start = 0, $filter = "all", $limit = 5, $pagination = 1, $meta = 0, $global_mode = 0) {
+    public function uvt_get_data($start = 0, $filter = "all", $limit = 5, $pagination = 1, $meta = 0, $global_mode = 0)
+    {
 
         global $wpdb;
         $bpvm_posts_data_table = $wpdb->prefix . "posts"; // for deatils. each day info.
@@ -242,7 +259,7 @@ class BPVM_UVT {
             $start = ($id - 1) * $limit;
         } else {
             $id = 0;
-//                        $start = 0;
+            //                        $start = 0;
         }
 
         // Add USER ID.
@@ -287,7 +304,7 @@ class BPVM_UVT {
             $vars[] = $post_type;
         }
 
-//                    $filter='all';
+        //                    $filter='all';
 
         if (isset($filter) && $filter != "all") {
             $con_mv_post_filters .= " AND {$bpvm_voting_data_table}.vote_type = %d ";
@@ -296,8 +313,8 @@ class BPVM_UVT {
 
         if ($uvt_custom_date_range == "true") {
 
-            $uvt_filter_start_date = ( $uvt_filter_start_date == "" ) ? "2010-04-01" : $uvt_filter_start_date;
-            $uvt_filter_end_date = ( $uvt_filter_end_date == "" ) ? "2020-04-01" : $uvt_filter_end_date;
+            $uvt_filter_start_date = ($uvt_filter_start_date == "") ? "2010-04-01" : $uvt_filter_start_date;
+            $uvt_filter_end_date = ($uvt_filter_end_date == "") ? "2020-04-01" : $uvt_filter_end_date;
 
             $con_mv_date_range_filters .= " AND DATE({$bpvm_voting_data_table}.vote_date) >= %s AND DATE({$bpvm_voting_data_table}.vote_date) <= %s ";
 
@@ -334,13 +351,13 @@ class BPVM_UVT {
 
         wp_reset_query();
 
-        $totalData = (!empty($bpvm_total_votes[0]['total_count']) ) ? $bpvm_total_votes[0]['total_count'] : 0;
+        $totalData = (!empty($bpvm_total_votes[0]['total_count'])) ? $bpvm_total_votes[0]['total_count'] : 0;
         $totalFiltered = ceil($totalData / $limit); // when there is no search parameter then total number rows = total number filtered rows.
         // Get each votes info in details. ( Details )
 
         $order_type = "DESC";
 
-//                    $order_query = " ORDER BY " . $columns[1] . "   " . $order_type . "  LIMIT " . $start . " ," . $limit . "   ";
+        //                    $order_query = " ORDER BY " . $columns[1] . "   " . $order_type . "  LIMIT " . $start . " ," . $limit . "   ";
         $order_query = " ORDER BY vote_date_time   " . $order_type . "  LIMIT " . $start . " ," . $limit . "   ";
 
         $sql = $wpdb->prepare("SELECT " . $bpvm_selected_columns . " FROM {$bpvm_voting_data_table}, {$bpvm_posts_data_table} 
@@ -360,15 +377,15 @@ class BPVM_UVT {
         if (sizeof($pvm_vote_data) > 0) :
 
             $uvt_output .= "";
- 
+
             foreach ($pvm_vote_data as $vote_data) :
-                
+
                 $post_title = $vote_data['post_title'];
                 $post_id = $vote_data['postid'];
                 $vote_type = $vote_data['vote_type'];
                 $voted_date = $vote_data['vote_date_time'];
 
-                $vote_type_text = ( $vote_type == 2 ) ? __('Disliked', 'bpvm_uvt') : __('Liked', 'bpvm_uvt');
+                $vote_type_text = ($vote_type == 2) ? __('Disliked', 'bpvm_uvt') : __('Liked', 'bpvm_uvt');
 
                 $uvt_post_link = get_permalink($post_id);
 
@@ -380,19 +397,18 @@ class BPVM_UVT {
 
                     $my_current_time = strtotime(date('Y-m-d H:i:s', current_time('timestamp')));
 
-                    $signed_date = strtotime( $voted_date );
+                    $signed_date = strtotime($voted_date);
 
-                    $voted_time_ago = human_time_diff( $signed_date, $my_current_time);
-                    
-                    $uvt_meta = '<span><time class="" datetime="'.$voted_date.'">' . $vote_type_text . ' ' . $voted_time_ago .  ' ' . __('ago', 'bpvm_uvt') .'</time></span>';
-                    
+                    $voted_time_ago = human_time_diff($signed_date, $my_current_time);
+
+                    $uvt_meta = '<span><time class="" datetime="' . $voted_date . '">' . $vote_type_text . ' ' . $voted_time_ago .  ' ' . __('ago', 'bpvm_uvt') . '</time></span>';
                 }
 
                 $uvt_output .= '<li><a href="' . $uvt_post_link . '">' . $post_title . '</a>' . $uvt_meta . '</li>';
 
             endforeach;
 
-        else:
+        else :
 
             $uvt_output .= '<li>' . __('Sorry, No Result Found !', 'bpvm_uvt') . '</li>';
 
@@ -426,7 +442,8 @@ class BPVM_UVT {
         return $uvt_output . $uvt_pagination_link;
     }
 
-    public function cb_uvt_front($atts) {
+    public function cb_uvt_front($atts)
+    {
 
         $atts = shortcode_atts(array(
             'filter' => 'all', // all, 1=liked, 2=disliked
@@ -434,7 +451,7 @@ class BPVM_UVT {
             'pagination' => 1,
             'meta' => 0,
             'global_mode' => 0
-                ), $atts);
+        ), $atts);
 
         extract($atts);
 
@@ -444,5 +461,4 @@ class BPVM_UVT {
 
         return $uvt_front_output;
     }
-
 }
